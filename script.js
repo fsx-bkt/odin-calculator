ce632b0fea168c1e1de5853c54bcffc62083942e
fsx-bkt <175029590+fsx-bkt@users.noreply.github.com>
2025-02-10 00:50:56 +1000

    Adding base function

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(){
+    console.log("Add works!");
+}
+
+function subtract(){
+    console.log("Subtract works!");
+}
+
+function multiply(){
+    console.log("Multiply works!");
+}
+
+function divide(){
+    console.log("Divide works!");
+}
+
+add();
+subtract();
+multiply();
+divide();
