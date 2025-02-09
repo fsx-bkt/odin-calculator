a9e5133b6222eebe0f228c679951ecfe6f9a92cb
fsx-bkt <175029590+fsx-bkt@users.noreply.github.com>
2025-02-10 00:55:07 +1000

    Added operate function to take inputs

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-function add(){
+function add(number1, number2){
     console.log("Add works!");
 }
 
-function subtract(){
+function subtract(number1, number2){
     console.log("Subtract works!");
 }
 
-function multiply(){
+function multiply(number1, number2){
     console.log("Multiply works!");
 }
 
-function divide(){
+function divide(number1, number2){
     console.log("Divide works!");
 }
 
-add();
-subtract();
-multiply();
-divide();
+function operate(operator, number1, number2){
+    switch(operator){
+        case '+':
+            add();
+            break;
+        case '-':
+            subtract();
+            break;
+        case '*':
+            multiply();
+            break;
+        case '/':
+            divide();
+            break;
+        default:
+            break;
+    }
+}
+
+operate('+',1,1);
+operate('-',1,1);
+operate('*',1,1);
+operate('/',1,1);
