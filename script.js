7b2ba6424ec411e307aef63f5d4cfcbf5cc8007b
fsx-bkt <175029590+fsx-bkt@users.noreply.github.com>
2025-02-10 22:18:22 +1000

    Added clear,equal, and add function

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let userInput = [];
+let userInput = ["","",""];
 let ifFirstNumberInput = true;
-let ifOperatorButtonPressed = true;
 
 let numberButtons = document.querySelectorAll(".number-button");
 let operatorButtons = document.querySelectorAll(".operator-button");
+let clearButton = document.querySelector(".clear-button");
+let calculateButton = document.querySelector(".calculate-button");
+let displayField = document.querySelector(".display");
 
 for (let i = 0; i < numberButtons.length; i++){
     numberButtons[i].addEventListener("click", assignNumberVariable)
@@ -11,23 +13,54 @@ for (let i = 0; i < numberButtons.length; i++){
 
 for (let i = 0; i < operatorButtons.length; i++){
     operatorButtons[i].addEventListener(("click"), assignOperatorVariable);
+    
 }
+
+clearButton.addEventListener("click", clear);
+calculateButton.addEventListener("click", operate);
 
 function assignNumberVariable(e){
     if(ifFirstNumberInput){
         userInput[0] += e.target.textContent;
+        updateDisplayField();
     }
     else{
         userInput[2] += e.target.textContent;
+        updateDisplayField();
     }
 }
 
 function assignOperatorVariable(e){
+    if(userInput[0] === ""){
+        alert("Please enter a number first!");
+        return;
+    }
     userInput[1] = e.target.textContent;
+    ifFirstNumberInput = false;
+    updateDisplayField();
+}
+
+function updateDisplayField(){
+    displayField.textContent = userInput[0] 
+    + userInput[1]
+    + userInput[2];
+}
+
+function clearInputAfterCalculation(){
+    userInput[1] = "";
+    userInput[2] = "";
+}
+
+function clear(){
+    userInput = ["","",""];
+    ifFirstNumberInput = true;
+    updateDisplayField();
 }
 
 function add(number1, number2){
-    console.log("Add works!");
+    userInput[0] = Number(number1) + Number(number2);
+    clearInputAfterCalculation();
+    updateDisplayField();
 }
 
 function subtract(number1, number2){
@@ -42,26 +75,30 @@ function divide(number1, number2){
     console.log("Divide works!");
 }
 
-function operate(operator, number1, number2){
+function operate(){
+    let operator = userInput[1];
+    let number1 = userInput[0];
+    let number2 = userInput[2];
+
+    if(number1 === "" || number2 === ""){
+        alert("Please enter 2 numbers to calculate!");
+        return;
+    }
+
     switch(operator){
         case '+':
-            add();
+            add(number1, number2);
             break;
         case '-':
-            subtract();
+            subtract(number1, number2);
             break;
         case '*':
-            multiply();
+            multiply(number1, number2);
             break;
         case '/':
-            divide();
+            divide(number1, number2);
             break;
         default:
             break;
     }
 }
-
-operate('+',1,1);
-operate('-',1,1);
-operate('*',1,1);
-operate('/',1,1);
